38b8212d9a946d0418b4bcb0ba02a29e76ee8871
thevishalhanda <vishalhanda0101@gmail.com>
2022-01-07 15:20:50 +0530

    NOTE

---
+++ src/components/filefor.tsx
@@ -0,0 +1,15 @@
+import {Component} from "react"
+
+class filefor extends Component {
+    render() {
+        return (
+            <div>
+                  <h1>
+                      DEATH NOTE
+                  </h1>
+            </div>
+        );
+    }
+}
+
+export default filefor;
---
+++ src/components/main.tsx
@@ -0,0 +1,15 @@
+import {Component} from "react";
+
+class MainComponents extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    HELLO WORLD   <br/>
+                    IS HERE
+                </h1>
+            </div>
+        );
+    }
+}
+export default MainComponents;
